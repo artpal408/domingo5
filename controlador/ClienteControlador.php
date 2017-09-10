48cd8c799c1037aafdd95e9faecfdb2a4f6e99d3
artpal408 <artpal2014@gmail.com>
2017-09-10 13:58:51 -0600

    domigno5

--- controlador/ClienteControlador.php
+++ controlador/ClienteControlador.php
@@ -1,3 +1,9 @@
 <?php
 
+require_once '../modelo/Cliente.modelo.php';
+
+$clienteModelo = new ClienteModelo();
+
+$mensaje = $clienteModelo->saludar();
+
 include_once '../vista/cliente.vista.php';
--- modelo/Cliente.modelo.php
+++ modelo/Cliente.modelo.php
@@ -1,3 +1,16 @@
 <?php
 
 require_once '../driver.php';
+
+class ClienteModelo{
+    private $enlace;
+
+    function __construct()
+    {
+        $this->enlace= new Dmysqli ();
+    }
+
+    function saludar (){
+        return 'ESTE MENSAJE VIENE DEL MODELO';
+    }
+}
